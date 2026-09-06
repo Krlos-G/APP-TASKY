465d862d706f7056e9dc274b571204fe78357941
Carlos Gabriel <carlosgabrielcaetanobarbosa@gmail.com>
2026-09-06 14:39:02 -0300

    feat(rotina): tela de montagem da rotina resumo

--- frontend/src/app/paginas/resumo/resumo.component.spec.ts
+++ frontend/src/app/paginas/resumo/resumo.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { Resumo } from './resumo.component';
 
 describe('Resumo', () => {
@@ -8,6 +9,7 @@ describe('Resumo', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Resumo],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Resumo);
--- frontend/src/app/paginas/resumo/resumo.component.ts
+++ frontend/src/app/paginas/resumo/resumo.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-resumo',
   styleUrl: './resumo.component.scss',
   templateUrl: './resumo.component.html',
